fd7019f547b15008d43ae6c7a3162d7e7c4ad7c0
chetanchadalavada <@Romans614>
2025-02-05 19:13:30 -0700

    make key private

--- app/api/openai.ts
+++ app/api/openai.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI({
     baseURL: 'https://api.deepseek.com',
