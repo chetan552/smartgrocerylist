1dc68e0ff4a41a814162fd55f8bcb5730c495826
Chetan Chadalavada <chetan.chadalavada@gmail.com>
2025-02-26 16:01:53 -0700

    Add google provider

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import { AuthOptions, getServerSession } from "next-auth"
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 const authOptions: AuthOptions = {
     // Configure one or more authentication providers
@@ -8,6 +9,17 @@ const authOptions: AuthOptions = {
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
         })
         // ...add more providers here
     ],
