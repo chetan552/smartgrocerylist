38e686cc20a5a26d7fd94df26164fb60630257c1
Chetan Chadalavada <chetan.chadalavada@gmail.com>
2025-02-27 10:15:09 -0700

    Add demo provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import { AuthOptions, getServerSession } from "next-auth"
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const authOptions: AuthOptions = {
     // Configure one or more authentication providers
@@ -18,6 +19,35 @@ const authOptions: AuthOptions = {
                     prompt: "consent",
                     access_type: "offline",
                     response_type: "code"
+                }
+            }
+        }),
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "demo" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                let user = null
+                if (credentials?.username == 'demo' && credentials?.password == 'demo@1983') {
+                    user = { id: "1", name: "Demo", email: "demo@example.com" }
+                }
+
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
                 }
             }
         })
